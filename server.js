eff09607fa15c369b079fcb37113a9930679aedd
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-05-09 11:20:05 -0700

    added seeds for the database and updated home routes and api routes as well as updated the homepage view

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const port = process.env.PORT || 3001;
 const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+
 const hbs = exphbs.create({});
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.listen(port, () => console.log(`Now listening on ${port}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
